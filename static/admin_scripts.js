183e6c908d37f36b1a281ab03dd667f5322c5d79
saelozahra <saelozahra14@gmail.com>
2022-09-01 02:42:54 +0430

    نمایش توضیحات مراحل اجرا

--- static/admin_scripts.js
+++ static/admin_scripts.js
@@ -6,11 +6,15 @@ $(document).ready(function(){
                 type: 'GET',
                 cache: false,
                 success: function(result) {
-                    //  alert(jQuery.dataType);
                     if (result) {
                         console.log(result.title);
                         //  var dd = JSON.parse(result);
                         $(".model-project .form-row").each(function(index,element){
+                            if(index >= 9 && index % 3 == 0){
+                                console.log(index / 3 - 3);
+                                $(this).find("label").text($(this).find("label").text()+" | "+result.marahel[(index / 3 - 3)]);
+                                // $(this).css({"color": "red", "background-color": "green"});
+                            }
                             if( index >= 9+(parseInt(result.marahel_count)*3 )){
                                 // console.log(index+": "+$(this).text());
                                 $(this).fadeOut();
