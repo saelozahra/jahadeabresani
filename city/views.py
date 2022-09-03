b224e7c903bd56b37e3fa220ae34edcf8b965e86
saelozahra <saelozahra14@gmail.com>
2022-09-03 23:55:05 +0430

    Complete City UI

--- city/views.py
+++ city/views.py
@@ -1,7 +1,9 @@
 from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import TemplateView
-import main
+
+import city.models
+import main.models
 from project.views import darsad_icon
 # Create your views here.
 
@@ -9,7 +11,7 @@ from project.views import darsad_icon
 class SingleCity(TemplateView):
     def get(self, request, slug, **kwargs):
         final_data = []
-        all_cities = main.models.CityProject.objects.filter(Q(slug__contains=slug))
+        all_cities = city.models.CityProject.objects.filter(Q(slug__contains=slug))
         projects_data = []
         this_city_projects = main.models.Project.objects.filter(Q(RelatedCity__slug__contains=slug))
 
@@ -19,6 +21,7 @@ class SingleCity(TemplateView):
                 'id': pd.id,
                 'title': pd.title,
                 'type':  pd.type,
+                'city':  pd.RelatedCity.city,
                 'photo':  pd.photo.url,
                 'pishrafte_kol':  pd.pishrafte_kol,
                 'date_start': pd.date_start,
@@ -34,6 +37,7 @@ class SingleCity(TemplateView):
                 'city': cd.city,
                 'slug': cd.slug,
                 'projects': projects_data,
+                'projects_count': projects_data.__len__(),
                 'miangin_pishraft': cd.miangin_pishraft,
                 'user_login': self.request.user.is_authenticated,
                 'user_id': self.request.user.id,
@@ -41,5 +45,5 @@ class SingleCity(TemplateView):
             }
 
         print(final_data)
-        return render(request, 'city.html', {'projects_data': final_data})
+        return render(request, 'city.html', final_data)
 
