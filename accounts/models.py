70f3ed364b7066ad70833009b72ad09290d1134e
saelozahra <saelozahra14@gmail.com>
2022-09-03 20:54:20 +0430

    add avatar

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    city = models.CharField(default="",null=True, blank=True, max_length=24, verbose_name="شهر")
-    address = models.CharField(default="",null=True, blank=True, max_length=202, verbose_name="نشانی منزل")
+    avatar = models.ImageField(default='', upload_to='files/avatar', verbose_name="تصویر نمایه")
+    city = models.CharField(default="", null=True, blank=True, max_length=24, verbose_name="شهر")
+    address = models.CharField(default="", null=True, blank=True, max_length=202, verbose_name="نشانی منزل")
     phone = models.CharField(default="", null=False, blank=False, max_length=11, verbose_name="شماره تماس")
     melli = models.CharField(default="", null=False, blank=False, max_length=10, verbose_name="کد ملی")
     is_project_admin = models.BooleanField(default=False, verbose_name="آیا مدیر پروژه هست؟")
