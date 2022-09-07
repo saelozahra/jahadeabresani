9a5de2357d29bf44c55adf8d7a6a691fdee46d26
saelozahra <saelozahra14@gmail.com>
2022-09-07 17:01:19 +0430

    حل مشکل لود شدن مراحل

--- static/js/admin_scripts.js
+++ static/js/admin_scripts.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
+
     $("#id_type").change(function(){
-        if($(this).val() !== ""){
+        update_data_ptype($(this));
+    });
+
+
+    function update_data_ptype(elem){
+
+        if(elem.val() !== ""){
             $.ajax({
-                url: "/api/project/type/" + $(this).val(),
+                url: "/api/project/type/" + elem.val(),
                 type: 'GET',
-                cache: false,
+                cache: true,
                 success: function(result) {
                     if (result) {
                         console.log(result.title);
                         //  var dd = JSON.parse(result);
                         $(".model-project .form-row").each(function(index,element){
-                            if(index >= 9 && index % 3 == 0){
+                            if(index >= 9 && index % 3 === 0){
                                 console.log(index / 3 - 3);
-                                $(this).find("label").text($(this).find("label").text()+" | "+result.marahel[(index / 3 - 3)]);
-                                $(this).find("label").css({"width": "100%"});
+                                if($(this).find("label").attr("first_run") === "false"){
+                                    $(this).find("label").text(result.marahel[(index / 3 - 3)]);
+                                    $(this).find("label").css({"width": "100%"});
+                                }else{
+                                    $(this).find("label").text($(this).find("label").text()+" | "+result.marahel[(index / 3 - 3)]);
+                                    $(this).find("label").css({"width": "100%"});
+                                }
+                                $(this).find("label").attr("first_run", "false");
                             }
                             if( index >= 9+(parseInt(result.marahel_count)*3 )){
                                 // console.log(index+": "+$(this).text());
@@ -29,6 +42,8 @@ $(document).ready(function(){
                 }
             });
         }
-    });
+    }
+
+    update_data_ptype($("#id_type"));
 
 });
